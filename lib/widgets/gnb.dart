08bc681f51ad1d68766e8403dcf35f6cbcbdbe31
전예진 <ezin@jeon-yejin-ui-MacBookPro.local>
2023-12-28 12:47:37 +0900

    refactor: GNB BottomNavigation → Tabbar 변경

--- lib/widgets/gnb.dart
+++ lib/widgets/gnb.dart
@@ -10,53 +10,99 @@ class GNB extends StatefulWidget {
 }
 
 class _GNBState extends State<GNB> {
-  int _selectedIndex = 0;
-
-  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text('Index 0: 홈', style: optionStyle),
-    Text('Index 1: 랭킹', style: optionStyle),
-    Text('Index 2: 새 피드 작성', style: optionStyle),
-    Text('Index 2: 피드', style: optionStyle),
-    Text('Index 2: 마이페이지', style: optionStyle),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-  
   //TODO: 아이콘 변경
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: '홈',
+    return Container(
+      color: Dark.black,
+      child: const SizedBox(
+        child: TabBar(
+          tabs: [
+            Tab(icon: Icon(Icons.home_outlined), text: '홈'),
+            Tab(icon: Icon(Icons.bar_chart_outlined), text: '랭킹'),
+            Tab(icon: Icon(Icons.add_circle_outline), text: '새 피드 작성'),
+            Tab(icon: Icon(Icons.feed_outlined), text: '피드'),
+            Tab(icon: Icon(Icons.account_circle_outlined), text: '마이페이지'),
+          ],
+          indicatorColor: Colors.transparent,
+          dividerHeight: 0,
+          labelColor: Main.main,
+          labelStyle: TextStyle(fontSize: 10),
+          unselectedLabelColor: Dark.gray400,
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.business),
-          label: '랭킹',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.school),
-          label: '새 피드 작성',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.school),
-          label: '피드',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.school),
-          label: '마이페이지',
-        ),
-      ],
-      currentIndex: _selectedIndex,
-      selectedItemColor: Main.main,
-      onTap: _onItemTapped,
+      ),
     );
   }
 }
+
+// class _GNBState extends State<GNB> {
+//   int _selectedIndex = 0;
+//
+//   static const List<Widget> _widgetOptions = <Widget>[
+//     HomeScreen(),
+//     RankScreen(),
+//     CreateFeedScreen(),
+//     FeedScreen(),
+//     MyPageScreen(),
+//   ];
+//
+//   static const List<String> pathnames = <String>[
+//     '/home',
+//     '/rank',
+//     '/feed',
+//     '/my-page'
+//   ];
+//
+//   void _onItemTapped(int index) {
+//     setState(() {
+//       _selectedIndex = index;
+//     });
+//     Navigator.pushNamed(context, pathnames[index]);
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return BottomNavigationBar(
+//       currentIndex: _selectedIndex,
+//       unselectedItemColor: Dark.gray400,
+//       selectedItemColor: Main.main,
+//       showUnselectedLabels: true,
+//       unselectedLabelStyle: const TextStyle(color: Dark.gray400),
+//       selectedFontSize: 10,
+//       unselectedFontSize: 10,
+//       onTap: _onItemTapped,
+//       items: const <BottomNavigationBarItem>[
+//         BottomNavigationBarItem(
+//           icon: Icon(Icons.home_outlined),
+//           activeIcon: Icon(Icons.home),
+//           backgroundColor: Dark.black,
+//           label: '홈',
+//         ),
+//         BottomNavigationBarItem(
+//           icon: Icon(Icons.bar_chart_outlined),
+//           activeIcon: Icon(Icons.bar_chart_rounded),
+//           backgroundColor: Dark.black,
+//           label: '랭킹',
+//         ),
+//         BottomNavigationBarItem(
+//           icon: Icon(Icons.add_circle_outline),
+//           backgroundColor: Dark.black,
+//           activeIcon: Icon(Icons.add_circle_rounded),
+//           label: '새 피드 작성',
+//         ),
+//         BottomNavigationBarItem(
+//           icon: Icon(Icons.feed_outlined),
+//           backgroundColor: Dark.black,
+//           activeIcon: Icon(Icons.feed_rounded),
+//           label: '피드',
+//         ),
+//         BottomNavigationBarItem(
+//           icon: Icon(Icons.account_circle_outlined),
+//           backgroundColor: Dark.black,
+//           activeIcon: Icon(Icons.account_circle_rounded),
+//           label: '마이페이지',
+//         ),
+//       ],
+//     );
+//   }
+// }
