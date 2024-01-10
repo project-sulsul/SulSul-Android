eb78061254a24fe0c139409ce6c97d959458b3c1
ez1n <eodrmfdl1004@naver.com>
2024-01-10 19:55:49 +0900

    refactor: 메인 화면 레이아웃 변경 & gnb

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -1,29 +1,82 @@
 import 'package:flutter/material.dart';
 
+import 'package:sul_sul/theme/colors.dart';
+
 import 'package:sul_sul/screens/sign_in_screen.dart';
 import 'package:sul_sul/screens/update_nickname_screen.dart';
-import 'package:sul_sul/screens/layout.dart';
+import 'package:sul_sul/screens/home_screen.dart';
 
-class MyApp extends StatelessWidget {
+import 'package:sul_sul/widgets/header.dart';
+
+import 'package:sul_sul/widgets/gnb.dart';
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomeScreen(),
+    Text('랭킹 화면을 추가해주세요.'),
+    Text('새 피드 작성 화면을 추가해주세요.'),
+    Text('피드 화면을 추가해주세요.'),
+    Text('마이페이지 화면을 추가해주세요.'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Sul Sul',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Dark.black,
+          iconTheme: IconThemeData(color: Dark.white),
+          titleTextStyle: TextStyle(color: Dark.white),
+        ),
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(
+            color: Dark.gray900,
+            fontSize: 15,
+          ),
+        ),
+        scaffoldBackgroundColor: Dark.black,
         useMaterial3: true,
       ),
-      home: const SignInScreen(),
+      themeMode: ThemeMode.dark,
+      home: Scaffold(
+        appBar: const Header(title: '술이 술술'),
+        body: SafeArea(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+        bottomNavigationBar: Theme(
+          data: ThemeData(
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+          ),
+          child: GNB(
+            index: _selectedIndex,
+            onTap: _onItemTapped,
+          ),
+        ),
+      ),
       routes: routes,
     );
   }
 }
 
 final routes = {
-  '/home': (context) => const Layout(),
   '/sign-in': (context) => const SignInScreen(),
-  '/preference': (context) => const SignInScreen(),
+  '/home': (context) => const HomeScreen(),
   '/update-nickname': (context) => const UpdateNicknameScreen(),
 };
