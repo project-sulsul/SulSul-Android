b27cf152d9843f34ff5ce1be103029604bad6847
ez1n <eodrmfdl1004@naver.com>
2024-01-13 16:09:47 +0900

    feat: 선호하는 술 / 함께 먹는 안주 api

---
+++ lib/models/preference_model.dart
@@ -0,0 +1,42 @@
+class PreferenceResponse {
+  final int id;
+  final String type;
+  final String subtype;
+  final String name;
+  final String description;
+  final bool isSelected;
+  final String? image;
+
+  PreferenceResponse({
+    required this.id,
+    required this.type,
+    required this.subtype,
+    required this.name,
+    required this.description,
+    required this.isSelected,
+    this.image,
+  });
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "type": type,
+        "subtype": subtype,
+        "name": name,
+        "image": image,
+        "description": description,
+        "isSelected": isSelected,
+      };
+
+  factory PreferenceResponse.fromJson(Map<String, dynamic> json) =>
+      PreferenceResponse(
+        id: json['id'],
+        type: json['type'],
+        subtype: json['subtype'],
+        name: json['name'],
+        // image: json['image'],
+        image:
+            'https://company.lottechilsung.co.kr/common/images/product_view0201_bh3.jpg',
+        description: json['description'],
+        isSelected: json['isSelected'] ?? false,
+      );
+}
