84d36d43339e7372237aed00fa00afae72ee9560
ez1n <eodrmfdl1004@naver.com>
2024-01-19 14:03:48 +0900

    feat: 함께 먹는 안주 List Item

---
+++ lib/widgets/food_card.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:easy_rich_text/easy_rich_text.dart';
+
+import 'package:sul_sul/theme/colors.dart';
+import 'package:sul_sul/theme/custom_icons_icons.dart';
+
+class FoodCard extends StatelessWidget {
+  final String subtype;
+  final String name;
+  final String search;
+  final int id;
+  final bool isSelected;
+  final void Function(int)? onTap;
+
+  const FoodCard({
+    super.key,
+    required this.subtype,
+    required this.name,
+    required this.id,
+    required this.isSelected,
+    this.search = '',
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        padding: const EdgeInsets.only(
+          top: 10,
+          bottom: 10,
+          left: 12,
+          right: 14,
+        ),
+        margin: const EdgeInsets.only(bottom: 5),
+        decoration: BoxDecoration(
+          color: isSelected ? Dark.gray100 : null,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            EasyRichText(
+              name,
+              defaultStyle: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+              patternList: [
+                EasyRichTextPattern(
+                  targetString: search,
+                  style: const TextStyle(
+                    color: Main.main,
+                  ),
+                ),
+              ],
+            ),
+            if (isSelected)
+              const Icon(
+                CustomIcons.check,
+                color: Dark.green050,
+                size: 10,
+              ),
+          ],
+        ),
+      ),
+      onTap: () => onTap!(id),
+    );
+  }
+}
