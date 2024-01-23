6eeaa1b46fc4fac963df5f46cbcd944f1c783835
ez1n <eodrmfdl1004@naver.com>
2024-01-23 13:59:59 +0900

    style: BlurContainer 스타일 변경

--- lib/widgets/blur_container.dart
+++ lib/widgets/blur_container.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 
+import 'package:sul_sul/theme/colors.dart';
+
 class BlurContainer extends StatelessWidget {
   final Widget child;
+  final double padding;
 
-  const BlurContainer({super.key, required this.child});
+  const BlurContainer({super.key, required this.child, this.padding = 0});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.symmetric(horizontal: padding),
       decoration: const BoxDecoration(
         boxShadow: [
           BoxShadow(
+            color: Dark.black,
             spreadRadius: 20,
-            blurRadius: 25,
+            blurRadius: 15,
           ),
         ],
       ),
