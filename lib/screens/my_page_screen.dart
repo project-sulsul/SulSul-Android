211c0bae2a7826b84681d89df64936937f1dd717
ez1n <eodrmfdl1004@naver.com>
2024-01-30 19:35:00 +0900

    feat: 마이페이지 TopActionBar 추가

--- lib/screens/my_page_screen.dart
+++ lib/screens/my_page_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+
+import 'package:sul_sul/utils/constants.dart';
+
+import 'package:sul_sul/widgets/top_action_bar.dart';
 import 'package:sul_sul/widgets/button.dart';
 
 class MyPageScreen extends StatelessWidget {
@@ -6,23 +10,28 @@ class MyPageScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          Button(
-            title: '닉네임 변경',
-            onPressed: () => Navigator.pushNamed(context, '/update-nickname'),
-          ),
-          Button(
-            title: '선호하는 술',
-            onPressed: () =>
-                Navigator.pushNamed(context, '/preference-alcohol'),
-          ),
-          Button(
-            title: '함께 먹는 안주',
-            onPressed: () => Navigator.pushNamed(context, '/preference-food'),
-          ),
-        ],
+    return Scaffold(
+      appBar: const TopActionBar(
+        action: ActionType.setting,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Button(
+              title: '닉네임 변경',
+              onPressed: () => Navigator.pushNamed(context, '/update-nickname'),
+            ),
+            Button(
+              title: '선호하는 술',
+              onPressed: () =>
+                  Navigator.pushNamed(context, '/preference-alcohol'),
+            ),
+            Button(
+              title: '함께 먹는 안주',
+              onPressed: () => Navigator.pushNamed(context, '/preference-food'),
+            ),
+          ],
+        ),
       ),
     );
   }
