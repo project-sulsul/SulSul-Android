f123f35acc6a40ca3214176b3fcb13da760c920d
ez1n <eodrmfdl1004@naver.com>
2024-02-01 11:27:29 +0900

    feat: 랭킹 순위 카드(TearCard) 술+안주

--- lib/widgets/rank/tear_card.dart
+++ lib/widgets/rank/tear_card.dart
@@ -3,20 +3,105 @@ import 'package:flutter/material.dart';
 import 'package:sul_sul/theme/colors.dart';
 
 class TearCard extends StatelessWidget {
-  final String name;
-  final String image;
+  final String alcoholName;
+  final String alcoholImage;
   final int rank;
+  final String? foodName;
+  final String? foodImage;
   final List<String>? tags;
   final void Function()? onTap;
 
   const TearCard({
     super.key,
-    required this.name,
-    required this.image,
+    required this.alcoholName,
+    required this.alcoholImage,
     required this.rank,
+    this.foodName,
+    this.foodImage,
     this.tags,
     this.onTap,
   });
+
+  Widget _name() {
+    if (foodName != null) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            alcoholName,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            '& $foodName',
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      );
+    }
+
+    return Column(
+      children: [
+        // TODO: 태그
+        if (tags != null)
+          const Row(
+            children: [],
+          ),
+        Text(
+          alcoholName,
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Dark.gray900,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _imageBadge(String image) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(27),
+      clipBehavior: Clip.hardEdge,
+      child: Image.network(
+        image,
+        width: 60,
+        height: 60,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _image() {
+    if (foodImage != null) {
+      return SizedBox(
+        height: 70,
+        child: Row(
+          children: [
+            Transform.translate(
+              offset: const Offset(54, 0),
+              child: _imageBadge(foodImage!),
+            ),
+            Transform.translate(
+              offset: const Offset(-54, 0),
+              child: _imageBadge(alcoholImage),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Image.network(
+      alcoholImage,
+      width: 70,
+      height: 70,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,10 +125,9 @@ class TearCard extends StatelessWidget {
                     style: const TextStyle(
                       fontSize: 24,
                       fontWeight: FontWeight.bold,
-                      color: Dark.gray900,
                     ),
                   ),
-                  // TODO: 순위 증가 / 감소
+                  // TODO: 순위 증가 / 감소 추가
                   const Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
@@ -61,31 +145,11 @@ class TearCard extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 4),
-              child: Image.network(
-                image,
-                width: 70,
-                height: 70,
-              ),
+              child: _image(),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 4),
-              child: Column(
-                children: [
-                  // TODO: 태그
-                  if (tags != null)
-                    const Row(
-                      children: [],
-                    ),
-                  Text(
-                    name,
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Dark.gray900,
-                    ),
-                  ),
-                ],
-              ),
+              child: _name(),
             ),
           ],
         ),
