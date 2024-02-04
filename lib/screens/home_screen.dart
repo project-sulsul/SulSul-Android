21d77c0d5fc1a1961918d42a940fd0ac7b8004fa
ez1n <eodrmfdl1004@naver.com>
2024-02-04 18:06:49 +0900

    feat: 피드 추천 카드 위젯 교체 (UI)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:easy_rich_text/easy_rich_text.dart';
+import 'package:provider/provider.dart';
 
+import 'package:sul_sul/models/preference_repository.dart';
+import 'package:sul_sul/providers/pairings_provider.dart';
+import 'package:sul_sul/utils/api/api_client.dart';
 
 import 'package:sul_sul/utils/constants.dart';
 import 'package:sul_sul/theme/colors.dart';
@@ -17,21 +21,11 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  PreferenceRepository preferenceRepository =
+      PreferenceRepository(apiClient: sulsulServer);
 
-  String selected = '';
-  List<PreferenceResponse> alcoholList = [];
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-
-  void _onSelectAlcohol(String alcohol) {
-    setState(() {
-      selected = alcohol;
-    });
-  }
+// FIXME: provider로 유저 데이터 받아오기 (취향 설정 여부)
+  bool preference = false;
 
   Divider _divider() {
     return const Divider(
@@ -41,16 +35,17 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Widget _title() {
+  Widget _title({required String text, String? target}) {
     return EasyRichText(
-      '$selected랑 어울리는\n안주로 골라봤어요!',
+      text,
       patternList: [
-        EasyRichTextPattern(
-          targetString: selected,
-          style: const TextStyle(
-            color: Main.main,
+        if (target != null)
+          EasyRichTextPattern(
+            targetString: target,
+            style: const TextStyle(
+              color: Main.main,
+            ),
           ),
-        ),
       ],
       defaultStyle: const TextStyle(
         fontSize: 28,
@@ -59,16 +54,23 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Widget _category() {
+  Widget _category({
+    required PairingsProvider provider,
+    required Set<String> alcoholList,
+    required String selected,
+  }) {
     return Row(
       children: [
         for (var alcohol in alcoholList)
           Button(
-            title: alcohol.name,
-            onPressed: () => _onSelectAlcohol(alcohol.name),
+            title: alcohol,
+            onPressed: () => provider.selectAlcohol(alcohol),
             size: ButtonSize.mini,
-            type:
-                selected == alcohol.name ? ButtonType.active : ButtonType.plane,
+            type: selected == alcohol ? ButtonType.active : ButtonType.plane,
+            padding: const EdgeInsets.symmetric(
+              vertical: 4,
+              horizontal: 24,
+            ),
           ),
       ],
     );
@@ -76,6 +78,10 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    var provider = context.watch<PairingsProvider>();
+    var categoryList = provider.alcoholCategory;
+    var selectedAlcohol = provider.selectedAlcohol;
+
     return Scaffold(
       appBar: const TopActionBar(
         action: ActionType.notice,
@@ -88,24 +94,67 @@ class _HomeScreenState extends State<HomeScreen> {
             children: [
               Padding(
                 padding: const EdgeInsets.only(
-                  bottom: 10,
                   left: 20,
                   right: 20,
                 ),
-                child: _title(),
+                child: preference
+                    ? _title(
+                        // TODO: 유저 닉네임 변경
+                        text: '보라색 하이볼님이 선택한\n취향으로 골라봤어요.',
+                        target: '보라색 하이볼',
+                      )
+                    : _title(
+                        text: '$selectedAlcohol이랑 어울리는\n안주로 골라봤어요!',
+                        target: selectedAlcohol,
+                      ),
               ),
-              if (alcoholList.isNotEmpty) // FIXME: 취향 설정한 경우
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: _category(),
+              if (!preference)
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 10,
+                    left: 20,
+                    right: 20,
+                  ),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: _category(
+                      provider: provider,
+                      alcoholList: categoryList,
+                      selected: selectedAlcohol,
+                    ),
+                  ),
                 ),
-              Recommendation(
-                alcohol: selected,
-                userName: 'hi',
-                isPreference: true,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Recommendation(
+                  alcohol: selectedAlcohol,
+                  isPreference: false,
+                ),
               ),
-              _divider()
+              _divider(),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      child: _title(text: '좋아요 많은 조합'),
+                    ),
+                  ],
+                ),
+              ),
+              _divider(),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      child: _title(text: '색다른 조합'),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
